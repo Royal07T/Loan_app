3da46e85255d8eae76df79abe09ab17664250127
Royal07T <timmyroyalty@gmail.com>
2025-03-25 11:31:03 +0100

    update

--- app/Http/Controllers/LoanAnalyticsController.php
+++ app/Http/Controllers/LoanAnalyticsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Loan;
-//use App\Http\Controllers\Charts;
+//use App\Http\Controllers\Controller;
 
 class LoanAnalyticsController extends Controller
 {
